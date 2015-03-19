9d8300c50e048a0b2e81526b6a7fab88af910c87
Dooley <nsewolo@gmail.com>
2015-03-19 00:07:56 -0400

    styles for ui elements

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,7 +2,7 @@
   'use strict';
 
   var app = document.querySelector('#app');
-  app.appName = 'Yo, Polymer App!';
+  app.appName = 'Polymer Mailer!';
 
   app.addEventListener('template-bound', function () {
     console.log('Our app is ready to rock!');
