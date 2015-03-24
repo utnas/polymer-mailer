d28295089ff5f789cca6fecba7440c72acdfc332
Dooley <nsewolo@gmail.com>
2015-03-24 11:12:29 -0400

    Email content added

---
+++ app/elements/email-element/js/email.js
@@ -0,0 +1,13 @@
+(function () {
+  Polymer({
+    email: {
+      id: 1,
+      from: "Albator",
+      to: "Rudy",
+      subject: "I will be back",
+      date: new Date(2014, 2, 20, 15, 30).toUTCString(),
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id ligula ac sem fringilla mattis. Nullam sodales mi vel semper volutpat. Phasellus lorem leo, luctus a lectus id, posuere aliquet orci. Praesent sit amet ipsum porttitor, tempus odio vel, bibendum mauris. Etiam magna lorem, rhoncus eget euismod ac, lobortis quis."
+    }
+  });
+
+})();
