dbaeb5432902e904663e3354dd363c8ec4731df4
Dooley <nsewolo@gmail.com>
2015-03-24 21:41:57 -0400

    transitions added

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,14 +5,19 @@
   // and give it some initial binding values
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
   var app = document.querySelector('#app');
-  app.appName = 'Yo, Polymer App!';
+  app.appName = 'Polymer Mailer';
 
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
-  app.addEventListener('template-bound', function() {
-    console.log('Our app is ready to rock!');
+  app.addEventListener('template-bound', function () {
+    console.log('Polymer mailer is ready to rock!');
   });
 
-// wrap document so it plays nice with other libraries
-// http://www.polymer-project.org/platform/shadow-dom.html#wrappers
+  var template = document.querySelector('template[is="auto-binding"]');
+  template.pages = [
+    {name: 'Single', hash: 'single'},
+    {name: 'page', hash: 'page'},
+    {name: 'app', hash: 'app'}
+  ];
+
 })(wrap(document));
